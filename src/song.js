68a0867b5535c7fe8debfc0591069c181f4d9420
Aurii <49456123+seenks1@users.noreply.github.com>
2020-06-13 02:02:49 +0700

    Create song.js

---
+++ src/song.js
@@ -0,0 +1,16 @@
+module.exports = /*abstract*/ class Song {
+  constructor(args) {
+    this.link = args.link;
+    this.id = args.id;
+    this.message = args.message;
+    this.title = args.title;
+    this.artist = args.artist;
+    this.thumbnail = args.thumbnail;
+    this.plays = args.plays;
+    this.duration = args.duration;
+  }
+
+  isValid() {
+    return this.message !== undefined && this.link !== undefined;
+  }
+};
